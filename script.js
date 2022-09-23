7ac7f22a671959c0b15de1d08720120b408bcccc
Shameed4 <94253513+Shameed4@users.noreply.github.com>
2022-09-23 10:34:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hello");
+var img = document.getElementById("image");
+var text = document.getElementById("text");
+
+function test() {
+    console.log("test");
+    img.style.display = "block";
+    text.style.display = "block";
+}
